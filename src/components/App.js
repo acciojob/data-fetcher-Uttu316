e638639267979320cb2d3a1ee91681cec84d4153
Utkarsh Gupta <34169292+Uttu316@users.noreply.github.com>
2025-03-01 21:59:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,27 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data,setData] = useState(null)
+
+  const getData = async ()=>{
+  const res =  await fetch('https://dummyjson.com/products')
+  const resData = await res.json();
+    setData(resData)
+  }
+  useEffect(()=>{
+      getData()
+},[])
   return (
     <div>
-        {/* Do not remove the main div */}
+    {!data && <p>Loading...</p>}
+  {!!data&&
+    <>
+       <h1>Data Fetched from API</h1>
+   <pre>
+       {JSON.stringify(data)}
+  </pre>
+    </>
+  }
     </div>
   )
 }
